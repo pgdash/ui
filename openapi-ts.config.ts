1dd6684d4d9ea93e2fac2ff8bde89a4e1a91181e
Mhirii <ahmedmhiri218@gmail.com>
2026-03-08 06:08:32 +0100

    add openapi

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -1,26 +1,25 @@
-
-import { defineConfig } from "@hey-api/openapi-ts";
+import { defineConfig } from "@hey-api/openapi-ts"
 
 export default defineConfig({
-  input: "./openapi.json",
-  output: [
-    {
-      postProcess: ["biome:format"],
-      path: "client",
-    },
-  ],
-  plugins: [
-    "@hey-api/typescript",
-    {
-      name: "@hey-api/sdk",
-      operations: {
-        strategy: "flat",
-      },
-    },
-    {
-      name: "@hey-api/schemas",
-      type: "json",
-    },
-    "@hey-api/client-ky",
-  ],
-});
+	input: "./openapi.json",
+	output: [
+		{
+			postProcess: ["biome:format"],
+			path: "client",
+		},
+	],
+	plugins: [
+		"@hey-api/typescript",
+		{
+			name: "@hey-api/sdk",
+			operations: {
+				strategy: "flat",
+			},
+		},
+		{
+			name: "@hey-api/schemas",
+			type: "json",
+		},
+		"@hey-api/client-ky",
+	],
+})
